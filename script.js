b8707c8e028b3b7ab46ac0c898baae8532514a7d
Ofish <vc.ofir@gmail.com>
2024-12-15 17:30:48 -0500

    added final touches

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-import { animate, scroll } from "https://cdn.jsdelivr.net/npm/motion@11.11.13/+esm";
+import { animate, scroll, inView, } from "https://cdn.jsdelivr.net/npm/motion@11.11.13/+esm";
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    animate(".title", { opacity: 1, rotate: 360 }, { duration: 1 });
-});
+
+document.querySelectorAll(".fade-animation").forEach((item) => {
+    scroll(animate(item, { opacity: [0, 1, 1, 0] }, { ease: "linear" }), {
+      target: item,
+      offset: ["start end", "end end", "start start", "end start"],
+    });
+  });
